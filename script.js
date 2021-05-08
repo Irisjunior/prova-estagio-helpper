91642519b8bfdc5391b19c617586b9f6b2f57604
Iris Leite Vieira Junior <irisjunior1@gmail.com>
2021-05-08 12:12:44 -0300

    ajuste do front pg list.html

--- script.js
+++ script.js
@@ -12,3 +12,48 @@ btnClear.addEventListener('click',function() {
       cidade = document.querySelector('#cidade');
       cidade.value =cidade.value.toUpperCase();
     }
+
+
+    function save() {
+    
+    let arrayPessoas=[]
+      if(JSON.parse(localStorage.getItem("pessoas")) != null){
+          arrayPessoas.push(JSON.parse(localStorage.getItem("pessoas")));
+      }
+
+      let nome = document.getElementById("nome").value;
+      let cpfcnpj = document.getElementById("cpfcnpj").value;
+      let telefone = document.getElementById("telefone").value;
+      let email = document.getElementById("email").value;
+      let cep = document.getElementById("cep").value;
+      let logradouro = document.getElementById("logradouro").value;
+      let numero = document.getElementById("numero").value;
+      let bairro = document.getElementById("bairro").value;
+      let estado = document.getElementById("estado").value;
+
+
+      var pessoa = {nome: nome, cpfcnpj: cpfcnpj, telefone: telefone, email: email, cep: cep,logradouro:logradouro,numero:numero,bairro:bairro,estado:estado};
+      
+      console.log(pessoa);
+
+
+      arrayPessoas.push(pessoa);
+var pessoaJson = JSON.stringify(arrayPessoas);
+localStorage.setItem("pessoas", pessoaJson);
+  
+      alert("Salvo com Sucesso");
+  }
+  
+  let lnome = document.getElementById('lnome')
+  lnome =localStorage.getItem("name")
+  
+  let btn = document.querySelector('.btn')
+
+  function getLocalStorage(){
+    btn.addEventListener('click', () => {
+      console.log(localStorage.getItem('pessoas.nome'))
+    })
+  }
+
+
+  
