441dbc22b3d31964181bea9042a477d83c639a18
Iris Leite Vieira Junior <irisjunior1@gmail.com>
2021-05-09 23:08:13 -0300

    ajustes

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# teste-pratico
-formulario de cadastro de clientes
+# Processo Seletivo da Helpper
+
+Aplicação desenvolvida para o processo seletivo da hellper,sugestões ?envie um E-mail irisjunior1@gmail.com
+
+
+## Tecnologias Usadas
+
+<ul>
+  <li>Html5</li>
+  <li>Bootstrap4</li>
+  <li>Javascript</li>
+  
+ 
+ </ul>
+
+### Imagens Demonstrativas
+<p align="center">
+  <img src="./img/Captura%20de%20tela%202021-05-09%20224814.jpg"width=400 height=200/>
+  <img src="./img/Captura%20de%20tela%202021-05-09%20225755.jpg"width=400 height=200/>
+  
+</p>
+
+
+##  Instalação
+
+Confira o passo a passo [clicando aqui](https://github.com/Irisjunior/prova-estagio-helpper) 
+
+Abra o seu terminal e rode os seguintes comandos:   
+
+- `git clone https://github.com/Irisjunior/prova-estagio-helpper.git` 
+- `cd prova-estagio-helpper` 
+- `code .` 
--- index.js
+++ index.js
@@ -2,7 +2,33 @@
 
 var dados = [] 
 
-function PopulaTabela(dados) {
+function ApagaRegistro(id) {
+  let _confirm = confirm("Deseja excluir este registro?")
+  if (_confirm) {
+    for(let i = 0; i < dados.length; i++){
+      if (dados[i].ID == id) {
+        dados.splice(i, 1) //--------------------funcao que apaga um ou mais elementos
+      }
+    }
+    PopulaTabela()
+  }
+}
+
+
+function EditaRegistro(id) {
+  $("#modalRegistro").modal("show")
+
+  dados.forEach(function (item) {
+    if (item.ID == id) {
+      $("#txtNome").val(item.Nome)
+      $("#txtE-mail").val(item.Email)
+      $("#txtTelefone").val(item.Telefone)
+      $("#txtCpfcnpj").val(item.Cpfcnpj)
+    }
+  })
+}
+
+function PopulaTabela() {
   if (Array.isArray(dados)) {
 
     localStorage.setItem("__dados__",JSON.stringify(dados))
@@ -23,9 +49,8 @@ function PopulaTabela(dados) {
       <td>${item.Estado}</td>
       <td>${item.Rua}</td>
       <td>${item.Numero}</td>
-      <td>${item.Logradouro}</td>
-      <td><button type="button" class="btn btn-primary"><i class="fa fa-edit"></button></td>
-      <td><button type="button" class="btn btn-danger"><i class="fa fa-trash"></button></td>
+      <td><button type="button" class="btn btn-primary" onclick="javascript:EditaRegistro(${item.ID});" ><i class="fa fa-edit"></button></td>
+      <td><button type="button" class="btn btn-danger" onclick="javascript:ApagaRegistro(${item.ID});" ><i class="fa fa-trash"></button></td>
       </tr>`)
     })
   }
